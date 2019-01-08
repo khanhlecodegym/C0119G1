f9e6006e85467eb862ba72ee5b3732cb83695be5
khanhtmd1997 <38125947+khanhtmd1997@users.noreply.github.com>
2019-01-09 00:04:24 +0700

    Edit and Add new file

--- Bai 1/function_nhom/file.js
+++ Bai 1/function_nhom/file.js
@@ -5,13 +5,27 @@ function khanh() {
   document.getElementById("Hi").value = res;
   var tuoi = 22;
   document.getElementById("age").value = tuoi;
-  document.getElementById("gt").value = "Nam";
+  var checkBoxNam = document.getElementById("nam");
+  var checkBoxNu = document.getElementById("nu");
+  var text = document.getElementById("gender");
+  if ((checkBoxNam.checked == true)&&(checkBoxNu.checked == true)
+    ||(checkBoxNam.checked == "") && (checkBoxNu.checked == "")) text.value = "";
+    else if(checkBoxNam.checked == true) text.value ="Nam";    
+        else document.getElementById("gender").value ="Nữ";
+
 }
-function nhom1() {
-  var str = "Nguyễn Văn Khánh + Hoàng Minh Chính + Bùi Thiện Tâm";
+function GroupsOne() {
+  var str = "Nguyễn Văn Khánh + Bùi Thiện Tâm + Hoàng Minh Chính";
   var res = str.toString();
-  document.getElementById("hello_nhom").value = res;
-  var tuoi = 22;
-  document.getElementById("age_tb").value = tuoi;
-  document.getElementById("gioitinh").value = "Nam";
+  document.getElementById("Hi_groups").value = res;
+  var tuoi = 24.6;
+  document.getElementById("age_medium").value = tuoi;
+  var checkBoxNam = document.getElementById("male");
+  var checkBoxNu = document.getElementById("female");
+  var text = document.getElementById("genneral_gender");
+  if ((checkBoxNam.checked == true)&&(checkBoxNu.checked == true)
+    ||(checkBoxNam.checked == "") && (checkBoxNu.checked == "")) text.value = "";
+    else if(checkBoxNam.checked == true) text.value ="Nam";    
+        else document.getElementById("genneral_gender").value ="Nữ";
+
 }
