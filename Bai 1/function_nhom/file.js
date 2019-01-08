f64605d97b1478b50f31fe8efcb4ba6632f83dc1
khanhtmd1997 <38125947+khanhtmd1997@users.noreply.github.com>
2019-01-08 16:16:54 +0700

    Add nhom 1

---
+++ Bai 1/function_nhom/file.js
@@ -0,0 +1,17 @@
+
+function khanh() {
+  var str = "Nguyễn Văn Khánh";
+  var res = str.toString();
+  document.getElementById("Hi").value = res;
+  var tuoi = 22;
+  document.getElementById("age").value = tuoi;
+  document.getElementById("gt").value = "Nam";
+}
+function nhom1() {
+  var str = "Nguyễn Văn Khánh + Hoàng Minh Chính + Bùi Thiện Tâm";
+  var res = str.toString();
+  document.getElementById("hello_nhom").value = res;
+  var tuoi = 22;
+  document.getElementById("age_tb").value = tuoi;
+  document.getElementById("gioitinh").value = "Nam";
+}
