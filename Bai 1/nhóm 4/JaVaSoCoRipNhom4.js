2623763ebff28e9f07a25770e560e42dd013d69d
unknown <hoquacvien@gmail.com>
2019-01-08 23:24:39 +0700

    Nhom 4 Say Oh Yeah

---
+++ Bai 1/nhóm 4/JaVaSoCoRipNhom4.js
@@ -0,0 +1,33 @@
+function changeMember(){
+document.getElementById("L1").innerHTML = "Trần Mạnh Long";
+document.getElementById("Tuoi").innerHTML = "31";
+var a =true;
+document.getElementById("1").innerHTML = a.sex();
+}
+function changeMember2(){
+document.getElementById("L1").innerHTML = "Hoàng Ngọc Phúc Long";
+document.getElementById("Tuoi").innerHTML = "22";
+var a =true;
+document.getElementById("1").innerHTML = a.sex();
+}
+function changeMember3(){
+document.getElementById("L1").innerHTML = "Hồ Quắc Viễn";
+document.getElementById("Tuoi").innerHTML = "22";
+var a =true;
+document.getElementById("1").innerHTML = a.sex();
+}
+function changeMember4(){
+document.getElementById("L1").innerHTML = "Trần Mạnh Long &nbsp;|&nbsp;Hoàng Ngọc Phúc Long &nbsp;|&nbsp;Hồ Quắc Viễn";
+	var Tuoi3=parseInt(31+22+22)/3;
+document.getElementById("Tuoi").innerHTML = +Tuoi3;
+var a =false;
+document.getElementById("1").innerHTML = a.sex()+"&nbsp;<p> Tại sao không phải là Nam 😭";
+}
+
+Boolean.prototype.sex = function() {
+  if (this.valueOf() == true) {
+    return "Nam";
+  } else {
+    return "Nữ";
+  }
+};
