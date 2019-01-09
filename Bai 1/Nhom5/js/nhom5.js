9e9e3f5970d853ce74b44eaa5097fce43f3c6019
anhdunghoang7895 <anhdunghoang7895@gmail.com>
2019-01-09 10:04:50 +0700

    update

---
+++ Bai 1/Nhom5/js/nhom5.js
@@ -0,0 +1,63 @@
+function Loc(){
+	var loc ="Trần Văn Lộc";
+	var dtb=loc.toString();
+	document.getElementById("dtb").value=loc;
+	var Tuoi ="22"
+	var dtbb=Tuoi.toString();
+	document.getElementById("dtbb").value=Tuoi;
+	var dtbb
+	var GioiTinh = document.getElementById("Nam");
+	GioiTinh.checked=true;
+}
+
+
+function Dung(){
+	var Dung ="Hoàng Anh Dũng";
+	var dtb=Dung.toString();
+	document.getElementById("dtb").value=Dung;
+	var Tuoi ="23"
+	var dtbb=Tuoi.toString();
+	document.getElementById("dtbb").value=Tuoi;
+	var dtbb
+	var GioiTinh = document.getElementById("Nam");
+	GioiTinh.checked=true;
+}
+
+
+function Hoang(){
+	var Hoang ="Nguyễn Thế Hoàng";
+	var dtb=Hoang.toString();
+	document.getElementById("dtb").value=Hoang;
+	var Tuoi ="22"
+	var dtbb=Tuoi.toString();
+	document.getElementById("dtbb").value=Tuoi;
+	var dtbb
+	var GioiTinh = document.getElementById("Nam");
+	GioiTinh.checked=true;
+}
+
+function Nhom5(){
+	var nhom ="Hoàng Anh Dung :Trân Văn Lộc: Nguyễn Thế Hoàng";
+	var dtb=nhom.toString();
+	document.getElementById("dtb").value=nhom;
+	var Tuoi ="23:22:22"
+	var dtbb=Tuoi.toString();
+	document.getElementById("dtbb").value=Tuoi;
+	var dtbb
+	var GioiTinh = document.getElementById("Nam");
+	GioiTinh.checked=true;}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
